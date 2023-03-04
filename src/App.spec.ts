a280d7989f18034f4c347ef588505aa2a29b7ed0
luisraphaelramer <luis-raphael.ramer@deutschebahn.com>
2023-03-04 18:18:54 +0100

    configure router, add some more basic tests for default layout

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -1,12 +1,22 @@
 import { shallowMount } from '@vue/test-utils';
 import { describe, expect, test } from 'vitest';
 import App from './App.vue';
+import DefaultLayout from './layouts/DefaultLayout.vue';
+
+function shallowMountApp() {
+    return shallowMount(App, {
+        global: { stubs: { RouterView: true } },
+    })
+}
 
 describe('App.vue', () => {
     test('should have the correct app heading', () => {
-        const cut = shallowMount(App, {
-            global: { stubs: { RouterView: true } },
-        })
+        const cut = shallowMountApp();
         expect(cut.get('h1').text()).toBe('b-informed')
+    })
+
+    test('should render default layout', () => {
+        const cut = shallowMountApp();
+        expect(cut.findComponent(DefaultLayout).isVisible()).toBe(true)
     })
 })
