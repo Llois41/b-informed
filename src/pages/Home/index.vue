40c0d3900d6450d7457aab6b0d0fd6ffe8acca3d
luisraphaelramer <luis-raphael.ramer@deutschebahn.com>
2023-05-14 20:15:01 +0200

    update packages, add auth, refactor routes

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,6 +1,27 @@
 <script setup lang="ts">
+import { useAuth0 } from '@auth0/auth0-vue';
+import { computed } from 'vue';
+
+const { loginWithRedirect: login, logout, user, isAuthenticated } = useAuth0();
+
+const isLoggedIn = computed(() => isAuthenticated.value);
+
+const authButtonText = computed(() => (isLoggedIn.value ? 'Logout' : 'Login'));
+
+async function onAuthButtonClicked() {
+  if (isLoggedIn.value) {
+    return await logout({ logoutParams: { returnTo: 'http://localhost:5173' } });
+  }
+  return login();
+}
 </script>
 
 <template>
   <h1>Welcome to HomePage</h1>
+  <div>
+    <button @click="onAuthButtonClicked">{{ authButtonText }}</button>
+    <div v-if="isLoggedIn">
+      <p>Welcome {{ user.name }}</p>
+    </div>
+  </div>
 </template>
